fbfb56e13343f9d1051c481068d5c4f6366cc72e
Herman Ryen Martinsen <hermanrm@vcvm18.idi.ntnu.no>
2021-12-06 12:03:27 +0100

    Fix path to base folders in experiment configs

--- configs/cbnet/experiments/cascade_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_1x_naplab_ms.py
+++ configs/cbnet/experiments/cascade_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_1x_naplab_ms.py
@@ -1,7 +1,7 @@
 _base_ = [
-    '../_base_/models/cascade_rcnn_swin_fpn.py',
-    '../_base_/datasets/coco_detection.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../../_base_/models/cascade_rcnn_swin_fpn.py',
+    '../../_base_/datasets/coco_detection.py',
+    '../../_base_/schedules/schedule_1x.py', '../../_base_/default_runtime.py'
 ]
 
 # Model settings
